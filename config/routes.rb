eb1c39ae99db1c96d706a311bc8748b0eb57225b
Seren <itoseren@gmail.com>
2021-12-14 04:28:13 +0000

    Add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :owner,skip: [:registrations, :passwords] ,controllers: {
+  sessions: "owner/sessions"
+}
+
+  devise_for :customers,skip: [:passwords,],controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

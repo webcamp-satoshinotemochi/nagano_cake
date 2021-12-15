9ee5adfeb9142e7c971b000cfe7f035939a38599
Seren <itoseren@gmail.com>
2021-12-15 08:21:03 +0000

    [Add routes]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  root to: 'public/homes#top'
-  get 'home/about' => 'public/homes#about'
-
   devise_for :owner,skip: [:registrations, :passwords] ,controllers: {
   sessions: "owner/sessions"
 }
@@ -12,5 +9,29 @@ Rails.application.routes.draw do
   sessions: 'public/sessions'
 }
 
+  namespace :owner do
+    root to: 'homes#top'
+    resources :items,except:[:destroy]
+    resources :genres,except:[:show,:destroy,:new]
+    resources :customers,except:[:new,:destroy]
+    resources :orders,only:[:show,:update]
+    resources :order_items,only:[:update]
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    resources :items,only:[:index,:show]
+    resources :cart_items,except:[:show,:new,:edit]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :orders,except:[:edit]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/thank' => 'orders#thank'
+    resource :customers,only:[:edit,:update]
+    get 'customers/mypage' => 'customers#mypage'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    resources :deliveries,except:[:show,:new]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

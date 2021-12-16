3d607e930e063695037276b0fb9ae3adcc5a5d0c
starones <itstarone172@gmail.com>
2021-12-16 13:05:13 +0000

    [Add]itemsのindexページ

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,9 @@
 class Public::ItemsController < ApplicationController
+
   def index
+    @total_items = Item.all
+    @items = Item.page(params[:page]).reverse_order
+    @genres = ItemGenre.all
   end
 
   def show

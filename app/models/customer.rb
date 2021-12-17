c1e870273ba169189609bb33369d95fa008e3707
EC2 Default User <kairyu.bonvivant@gmail.com>
2021-12-17 15:26:31 +0000

    [Add 注文情報入力画面レイアウト]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,19 +3,17 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :cart_items
   has_many :items, through: :cart_items
-  
+
   has_many :deliveries
   has_many :orders
-  
+
   validates :family_name,presence:true
   validates :first_name,presence:true
   validates :family_name_kana,presence:true
   validates :first_name_kana,presence:true
   validates :post_address,presence:true,length: { is: 7 }
-  validates :family_name,presence:true
-  validates :family_name,presence:true
 
 end

6e6ccc198d4aee1bede4ba311bdb507736a8df5b
Seren <itoseren@gmail.com>
2021-12-17 10:25:47 +0000

    [Add]管理者商品一覧、商品新規作成ページ

--- app/controllers/owner/genres_controller.rb
+++ app/controllers/owner/genres_controller.rb
@@ -1,15 +1,24 @@
 class Owner::GenresController < ApplicationController
-  
+
   def index
+    @genres = Genre.all
+    @genre = Genre.new
   end
-  
+
   def edit
+    @genre = Genre.find(genre_params)
   end
-  
+
   def create
+
   end
-  
+
   def update
   end
-  
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end

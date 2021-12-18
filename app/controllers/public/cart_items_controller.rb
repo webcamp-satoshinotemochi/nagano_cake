03d11dfbcfbd992ed4e6ad3736df727edef86aab
starones <itstarone172@gmail.com>
2021-12-18 09:03:24 +0000

    [Add]controller/create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,10 +2,20 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
-    @total = @cart_items.inject(0) { |sum, item| sum + item.subtoal }
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+       cart_item.quantity += params[:cart_item][:quantity].to_i
+       cart_item.save
+       redirect_to cart_items_path
+    else
+       @cart_item.save
+       redirect_to cart_items_path
+    end
   end
 
   def update

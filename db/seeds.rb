f1553239fd1c02b87326cf0f381b279d4672d1cf
starones <itstarone172@gmail.com>
2021-12-18 03:59:42 +0000

    [Add]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Owner.create!(
+  email: 'a@a',
+  password: 'naganocake'
+  )
+
+
+  Genre.create(
+  [{
+      name: "ケーキ"
+  }]
+  )

8cd4e94b8bd9b5f596c1d492114f62899fb82775
starones <itstarone172@gmail.com>
2021-12-18 07:56:48 +0000

    [Add]View

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,26 @@
 class Public::CartItemsController < ApplicationController
+
   def index
+    @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtoal }
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destory
+  end
+
+  def destroy_all
+  end
+
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :quantity)
+  end
+
 end

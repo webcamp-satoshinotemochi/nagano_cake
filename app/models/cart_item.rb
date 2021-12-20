84d8dac812e4cb234dcc0460c4aeaea39fdc6c96
starones <itstarone172@gmail.com>
2021-12-20 12:53:15 +0000

    [Fix]円の追加とcart_itemの注文バリデーション

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,9 @@ class CartItem < ApplicationRecord
 
   belongs_to :customer
   belongs_to :item
+  
+  validates :quantity, presence: true
+
   # --------小計をもとめるメソッド---------
   def subtotal
     item.with_tax_price*quantity

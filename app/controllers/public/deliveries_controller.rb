c39f3c27b1849e1e6117d2ca3a5004444d3784cf
starones <itstarone172@gmail.com>
2021-12-20 03:13:03 +0000

    [Add]編集機能

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -6,12 +6,23 @@ def index
 end
 
 def create
+  delivery = Delivery.new(delivery_params)
+  delivery.customer_id = current_customer.id
+  if delivery.save
+    redirect_back(fallback_location:root_path)
+  else
+    redirect_to request.referer
+  end
 end
 
 def destroy
+  delivery = Delivery.find(params[:id])
+  delivery.destroy
+  redirect_back(fallback_location:root_path)
 end
 
 def edit
+  @delivery = Delivery.find(params[:id])
 end
 
 def update

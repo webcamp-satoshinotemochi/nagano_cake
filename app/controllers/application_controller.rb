3efa3f08396fa25cd2dbe234dd2ad4be368695b9
EC2 Default User <kairyu.bonvivant@gmail.com>
2021-12-20 09:37:34 +0000

    [Fix 細かい仕様の不全]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,20 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name,:first_name,:family_name_kana,:first_name_kana,:post_address,:address,:phone_number])
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Owner)
+      owner_root_path
+    else
+      mypage_customers_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :owner
+      new_owner_session_path
+    else
+      root_path
+    end
+  end
 end
